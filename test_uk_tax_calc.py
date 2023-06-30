8be7e6830ad9a3e8e35a6fdee2cfda172f810fa7
Pauline Korukundo <pktpaulie@gmail.com>
2023-06-30 11:54:46 +0300

    ADD TEST CYCLE 3 Define test for third function Run green test for values between 0 and 12001

--- test_uk_tax_calc.py
+++ test_uk_tax_calc.py
@@ -2,23 +2,39 @@
 Function to test the tax calculation solution in uk_tax_calc.py
 """
 
-from uk_tax_calc import tax_calc1, tax_calc2
+from uk_tax_calc import tax_calc1, tax_calc2, tax_calc3
 
-# 1. Define first failing test for the tax calculator method
-# 2. Define the tax calculation and get a GREEN test
+"""TEST CYCLE 1"""
+
 def test_tax_calc1():
+    # 1. Define first failing test for the tax calculator method
     assert tax_calc1(1) == 0
+
 
 # define second test
 def test_tax_calc2():
+    # 2. Define the tax calculation and get a GREEN test
     assert tax_calc1(2000) == 0
 
-# test other values including negatives
+
+"""TEST CYCLE 2 """
+
 def test_tax_calc3():
+    # test other values including negatives
     assert tax_calc2(1) == 0
+
 
 def test_tax_calc4():
     assert tax_calc2(2000) == 0
 
+
 def test_tax_calc5():
+    # test negatives
     assert tax_calc2(-2000) == None
+
+
+def test_tax_calc6():
+    """TEST CYCLE 3"""
+    assert tax_calc3(12000) == 0
+
+
--- uk_tax_calc.py
+++ uk_tax_calc.py
@@ -3,7 +3,9 @@ Function to get the income tax imposed on ones earnings
 in the UK
 
 consider: 
-
+If earnings < 12000 per year, pay no tax
+then with earnings between 12,000 and 36,000 pay 20% tax
+then with earnings greater than 36000 per year, pay 40% tax
 
 """
 
@@ -15,3 +17,11 @@ def tax_calc2(income):
         return 0
     else:
         pass
+
+def tax_calc3(income):
+    if income <= 12000:
+        tax = 0
+    else:
+        pass
+    return tax
+
