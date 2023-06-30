2f6499e9386447ded4bb7f025e0c82e695c07e40
Pauline Korukundo <pktpaulie@gmail.com>
2023-06-30 11:32:44 +0300

    ADD TEST CYCLE 2 Define test for second function Run green tests Check negative values and small values

--- test_uk_tax_calc.py
+++ test_uk_tax_calc.py
@@ -2,7 +2,7 @@
 Function to test the tax calculation solution in uk_tax_calc.py
 """
 
-from uk_tax_calc import tax_calc1
+from uk_tax_calc import tax_calc1, tax_calc2
 
 # 1. Define first failing test for the tax calculator method
 # 2. Define the tax calculation and get a GREEN test
@@ -13,3 +13,12 @@ def test_tax_calc1():
 def test_tax_calc2():
     assert tax_calc1(2000) == 0
 
+# test other values including negatives
+def test_tax_calc3():
+    assert tax_calc2(1) == 0
+
+def test_tax_calc4():
+    assert tax_calc2(2000) == 0
+
+def test_tax_calc5():
+    assert tax_calc2(-2000) == None
--- uk_tax_calc.py
+++ uk_tax_calc.py
@@ -10,3 +10,8 @@ consider:
 def tax_calc1(income):
     return 0
 
+def tax_calc2(income):
+    if income > 0:
+        return 0
+    else:
+        pass
